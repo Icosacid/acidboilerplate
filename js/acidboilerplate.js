8eea8eb7c4c703bb9a2141a8808edeae1e46b416
Alexandre Andrieux <alex@icosacid.com>
2016-07-13 11:08:50 +0200

    Initial commit: quick&dirty boilerplate including Bootstrap vendor folder

---
+++ js/acidboilerplate.js
@@ -0,0 +1,16 @@
+/**
+ * JS for I don't know what yet
+ * @author Alexandre Andrieux <alex@icosacid.com>
+ * @since 2015-07
+ */
+
+// Globals
+
+
+/**
+ * Initializer
+ */
+jQuery(document).ready(function() {
+	console.log('Mmmeh');
+});
+
